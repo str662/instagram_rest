d8bbc7af34e7ac475a2115180fc6cd5890dd347f
str <timkakadirov96@gmail.com>
2024-08-10 09:15:57 +0500

    2 lesson

--- users/models.py
+++ users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from shared.models import BaseModel
+from datetime import datetime, timedelta
+import random
+from django.core.validators import FileExtensionValidator
 
 ORDINARY_USER, MANAGER, ADMIN = 'ordinary_user', 'manager', 'admin'
 
@@ -33,5 +36,47 @@ class UserModel(AbstractUser, BaseModel):
     auth_status = models.CharField(max_length=50, choices=AUTH_STATUS, default=NEW)
     email = models.EmailField(max_length=254, null=True, blank=True, unique=True)
     phone_number = models.CharField(max_length=13, null=True, blank=True, unique=True)
-    photo = models.ImageField(upload_to='users_image/', null=True, blank=True)
+    photo = models.ImageField(upload_to='users_image/', null=True, blank=True, 
+                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'heic', 'heif'])])
     
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def create_verify_code(self, verify_type):
+        code = "".join([str(random.randint(0, 10000) % 10)for _ in range(4)])
+        UserConfirmation.objects.create(
+            user_id = self.id,
+            verify_type = verify_type,
+            code = code
+        )
+
+        return code
+
+
+PHONE_EXPIRE = 2
+EMAIL_EXPIRE = 5
+
+class UserConfirmation(BaseModel):
+
+    TYPE_CHOICES = (
+        (VIA_PHONE, VIA_PHONE),
+        (VIA_EMAIL, VIA_EMAIL)
+    )
+
+    code =models.CharField(max_length=4)
+    verify_type = models.CharField(max_length=50, choices=TYPE_CHOICES)
+    user = models.ForeignKey("users.UserModel", on_delete=models.CASCADE, related_name='veriry_codes')
+    expiration_time = models.DateTimeField(null=True)
+    is_confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.user.__str__())
+
+    def save(self, *args, **kwargs):
+        if not  self.pk:
+            if self.verify_type == VIA_EMAIL:
+                self.expiration_time = datetime.now() + timedelta(minutes=EMAIL_EXPIRE)
+            else:
+                self.expiration_time = datetime.now() + timedelta(minutes=PHONE_EXPIRE)
+        super(UserConfirmation, self).save(*args, **kwargs)
